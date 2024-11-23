bae72154aaf26ef1119611a5544aaaa00648b297
mxrsel <marsel14082006@gmail.com>
2024-11-23 11:58:41 +0600

    added styles for toolbar

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -2,10 +2,14 @@ import {NavLink} from "react-router-dom";
 
 const Toolbar = () => {
     return (
-        <div className='navbar'>
-            <NavLink to='/' className='navbar-brand'>Finance Tracker</NavLink>
-            <NavLink to='/categories' className='navbar-brand'>Categories</NavLink>
-            <NavLink to='/newTransaction' className='navbar-brand'>Add</NavLink>
+        <div className='container mt-3'>
+        <div className='navbar bg-dark p-2' style={{borderRadius: '10px'}}>
+            <NavLink to='/' className='navbar-brand text-light'>Finance Tracker</NavLink>
+            <div>
+            <NavLink to='/categories' className='nav-item me-3 text-decoration-none text-light'>Categories</NavLink>
+            <NavLink to='/newTransaction' className='nav-item text-decoration-none text-light'>Add</NavLink>
+            </div>
+        </div>
         </div>
     );
 };
